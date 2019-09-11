b3486f62aa16013f11c11e3df54ee42eed231d70
Shefali Lachhar <shefali.lachhar@gmail.com>
2019-09-11 23:37:49 +0530

    Generics with constraints

---
+++ Generics/List.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class GenericList<T> where T : IComparable
+    {
+        public void Add(T name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T this[int index]
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public T Max(T a, T b)
+        {
+            return a.CompareTo(b) > 0 ? a : b;
+        }
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -10,7 +10,8 @@ namespace Generics
     {
         static void Main(string[] args)
         {
-
+            var list = new GenericList<string>();
+            list.Add("shefali");
         }
     }
 }
