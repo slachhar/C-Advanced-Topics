371f6d7f7e3b9e61bf8454371c8f6808a96fdadc
Shefali Lachhar <shefali.lachhar@gmail.com>
2019-09-13 10:09:01 +0530

    Delegates as extensible methods

---
+++ Delegates/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processor = new PhotoProcessor();
+            var filter = new PhotoFilters();
+            PhotoProcessor.PhotoFilterHandler filterHandler = filter.ApplyBrightness;
+            filterHandler += filter.ApplyContrast;
+            processor.Process("", filterHandler);
+            Console.ReadLine();
+        }
+    }
+}
