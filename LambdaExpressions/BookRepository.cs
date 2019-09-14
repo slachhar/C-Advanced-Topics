e0226f478cc3021672a49395fc30ef4301bcbe1e
Shefali Lachhar <shefali.lachhar@gmail.com>
2019-09-14 17:06:18 +0530

    How are Lambda Expressions flexible and convinient

---
+++ LambdaExpressions/BookRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressions
+{
+    class BookRepository
+    {
+        public List<Book> GetBooks()
+        {
+            return new List<Book>()
+            {
+                new Book() {Price = 5, Title = "To kill a mocking bird"},
+                new Book() {Price = 10, Title = "The Lean StartUp"},
+                new Book() {Title = "1984", Price = 14.2M}
+            };
+        }
+
+    }
+}
---
+++ LambdaExpressions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BookRepository rep = new BookRepository();
+            var books = rep.GetBooks();
+            var cheapBooks = books.FindAll(CheaperThan10Dollar);
+            foreach (var book in cheapBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+            Console.Read();
+            ////Func<int, int> Square = number => number * number;
+            ////Console.WriteLine(Square(10));
+        }
+
+        static bool CheaperThan10Dollar(Book book)
+        {
+            return book.Price < 10;
+        }
+    }
+}
